9c7465e6a945ece318b14cc2d08d232694bed090
Mohit sahu <mohitsahu60067@gmail.com>
2025-01-04 16:02:47 +0530

    done

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function heighScore(){
     if (level > heighestScore) {
         heighestScore = level;  // Update highest score
     }
-    head.innerHTML += `<br>Your highest score is <b>${heighestScore}</b>`; // Display highest score
+    head.innerHTML = `Level ${level}<br>Your highest score is <b>${heighestScore}</b> Press any key to restart`;// Display highest score
 }
 
 
@@ -27,20 +27,20 @@ function reset(){
 }
 
 function checkAns(idx){
-if(gameSeq[idx] === userSeq[idx]){
-    if(userSeq.length == gameSeq.length){
-        setTimeout(levelUp, 1000);
+    if(gameSeq[idx] === userSeq[idx]){
+        if(userSeq.length == gameSeq.length){
+            setTimeout(levelUp, 1000);
+        }
     }
-}
-else{
-    head.innerHTML = `Game over! your scroe was <b>${level}</b> <br> press any key to restart`;
-    document.querySelector("body").style.backgroundColor = "red";
-    setTimeout(() => {
-        document.querySelector("body").style.backgroundColor = "white";
-    }, 150);
-    heighScore();
-    reset();
-}
+    else{
+        head.innerHTML = `Game over! your scroe was <b>${level}</b> <br> press any key to restart`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(() => {
+            document.querySelector("body").style.backgroundColor = "white";
+        }, 150);
+        heighScore();
+        reset();
+    }
 }
 
 function userFlash(btn){
@@ -74,20 +74,20 @@ function gameFlash(btn){
 }
 
 function levelUp(){
-userSeq = [];
-level++;
-head.innerText = `Level ${level}`; 
+    userSeq = [];
+    level++;
+    head.innerText = `Level ${level}`; 
 
-let randomIdx = Math.floor(Math.random() * 4);
-let randomColor = btns[randomIdx];
-let randomBtn = document.querySelector(`.${randomColor}`);
-gameSeq.push(randomColor);
-console.log(gameSeq);
-gameFlash(randomBtn);
+    let randomIdx = Math.floor(Math.random() * 4);
+    let randomColor = btns[randomIdx];
+    let randomBtn = document.querySelector(`.${randomColor}`);
+    gameSeq.push(randomColor);
+    console.log(gameSeq);
+    gameFlash(randomBtn);
 
 }
 
-document.addEventListener("click" , () => {
+document.addEventListener("keypress" , () => {
     if(gameStart == false){//game start
         gameStart = true;
     }
